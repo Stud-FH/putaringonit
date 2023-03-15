51205145ef378d695e3d300915cd4186321164d9
Florian <herzogflorian95@gmail.com>
2023-03-15 13:10:28 +0100

    cors

--- src/main/java/ch/bookoflies/putaringonit/PutaringonitApplication.java
+++ src/main/java/ch/bookoflies/putaringonit/PutaringonitApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,21 +24,23 @@ public class PutaringonitApplication {
         SpringApplication.run(PutaringonitApplication.class, args);
     }
 
-
     @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
     public String helloWorld() {
         return "Hello World " + LocalDateTime.now();
     }
 
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        System.out.println("test");
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("localhost:4200", "https://putaringonit-angular.oa.r.appspot.com/api", "https://s-und-f.herokuapp.com/")
-                        .allowedMethods("*");
+                        .allowedHeaders("*")
+                        .allowedOrigins("**", "https://putaringonit-angular.oa.r.appspot.com/api", "https://s-und-f.herokuapp.com/")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
             }
         };
     }
